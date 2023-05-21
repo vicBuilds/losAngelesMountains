76412c469d07acf9b20bf8fb09c22b031ef0bf23
Victor Mitra <victor.mitra15@gmail.com>
2023-05-21 23:53:42 +0530

    Everything seem to work fine. Lets write the media queries

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,35 @@
-console.log("Hello I am connected");
+const carousel = document.querySelector(".carousel");
+const cardsContainer = carousel.querySelector(".cards-container");
+const dots = Array.from(carousel.querySelectorAll(".dot"));
+const cardWidth = carousel.offsetWidth / 3;
+let currentIndex = 0;
+
+dots.forEach((dot, index) => {
+  dot.addEventListener("click", () => {
+    currentIndex = index;
+    updateCarousel();
+  });
+});
+
+function updateCarousel() {
+  const offset = -currentIndex * cardWidth * 3;
+  cardsContainer.style.transform = `translateX(${offset}px)`;
+
+  dots.forEach((dot) => dot.classList.remove("active"));
+  dots[currentIndex].classList.add("active");
+}
+
+// Initialize carousel
+updateCarousel();
+
+const mountainLinks = document.querySelectorAll(".mountain-link");
+const mountains = document.querySelectorAll(".mountain");
+
+mountainLinks.forEach((link) => {
+  link.addEventListener("click", function () {
+    const target = this.dataset.target;
+    mountains.forEach((mountain) => {
+      mountain.style.display = mountain.id === target ? "block" : "none";
+    });
+  });
+});
